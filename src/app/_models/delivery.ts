1d9bbd0b975cce05bf3e1763df3ac35572c2fb05
Barak Segal <barak353@gmail.com>
2019-01-06 23:37:44 +0200

    Can get adress and transfer it to lan and att. (This commit is not working, it's needs fix)

--- src/app/_models/delivery.ts
+++ src/app/_models/delivery.ts
@@ -2,6 +2,7 @@
     id: number;
     name: string;
     isUrgent: string;
+    adress: string;
     latitude: string;
     longitude: string;
     type: string;//The server will choose the type of the delivery (defualt is 0).
